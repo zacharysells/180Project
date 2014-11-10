eab823a2764664f8886eb0eb4e17095780f63a44
mcole004 <mcole004@ucr.edu>
2014-11-10 12:30:30 -0800

    Update application_controller.rb

--- ucr_hbs/app/controllers/application_controller.rb
+++ ucr_hbs/app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def StoreSearchParams(params)
+    @search_params = params
+  end
+  
+  #def after_sign_in_path_for(resource)
+  #  profile_path(resource)
+  #end
+
+  #def after_sign_up_path_for(resource)
+  #  profile_path(resource)
+  #end
+  
 end
