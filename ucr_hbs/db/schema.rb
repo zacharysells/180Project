6d1531177f98dc69ccafae94a7751d67709c100d
mcole004 <mcole004@ucr.edu>
2014-11-10 12:55:13 -0800

    Update schema.rb

--- ucr_hbs/db/schema.rb
+++ ucr_hbs/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141022211939) do
+ActiveRecord::Schema.define(version: 20141110061425) do
+
+  create_table "reviews", force: true do |t|
+    t.string   "hotel"
+    t.decimal  "rating"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
